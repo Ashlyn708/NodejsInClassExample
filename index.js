5e41f96bc7dce2100faf1d72e7f00d52e6c24167
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-09-22 23:00:22 +0000

    in class Node stuff

---
+++ index.js
@@ -0,0 +1,16 @@
+//require express
+var express = require('express');
+
+//create express object, call express
+var app = express();
+
+//get home page /
+app.get('/', function(req, res){
+    //return something to home page
+    res.send('Hello World');
+});
+
+//server setup
+app.listen(3000, function(){
+    console.log('Listening!')
+});

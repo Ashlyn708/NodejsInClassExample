756a4752ad19f96c23aa0168360c4810a5df5603
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-10-01 22:11:35 +0000

    completed stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //require express
 var express = require('express');
 //require body-paser
-var body = require("body-parser");
+var bodyParser = require("body-parser");
 
 //create express object, call express
 var app = express();
@@ -14,19 +14,39 @@ app.use(bodyParser.urlencoded({extended:true}))
 //couple of items items
 var tasks=["make it to class","take child to daycare"];
 
+var completed=["extrawork"];
+
 //get home page /
 app.get('/', function(req, res){
     //return something to home page
-    res.render('index');
+    res.render('index',{task:task});
+    res.render('index',{completed:completed});
 });
 
 //add post method/ addtask
 app.post('/addtask', function(req,res){
     var newTask = req.body.newTask;
-    console.log("newTask")
+    console.log("newTask");
+    tasks.push(newTask);
     //return index
     res.render('index');
 });
+
+app.post('/removetask', function(req, res){
+    var removeTask = req.body.check;
+    //push to completed
+    if(typeof removeTask === 'string'){
+        tasks.splice(tasks.indexOf(removeTask), 1);
+        completed.push(removeTask);
+    }else if(typeof removeTask === 'object'){
+        for (var i = 0; i < removeTask.length; i++){
+            tasks.splice(tasks.indexOf(removeTask[i]), 1);
+        }
+    }
+    res.redirect('/');
+});
+
+app.post()
 
 //server setup
 app.listen(3000, function(){
